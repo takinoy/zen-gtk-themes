19a66609dc353cef1fd5721e72fbc563f8b443f0
cedric <cedl38@gmail.com>
2013-04-01 18:23:53 +0200

    update link.sh

--- link.sh
+++ link.sh
@@ -4,9 +4,7 @@
 
 # package name
 pkgname=$(grep -m 1 'pkgname=' cache/PKGBUILD | cut -d= -f2)
-name=${pkgname%%-*}
-# first letter in uppercase
-name=$(echo ${name:0:1}|tr a-z A-Z)${name:1}
+name=ZenX
 
 # directory
 theme_dir="$HOME/.themes"
