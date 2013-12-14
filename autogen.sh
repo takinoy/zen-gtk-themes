ca42b54be9461a000e875984b6ffa04da8f8dc7b
Cédric Leporcq <cedl38@gmail.com>
2013-12-14 09:49:39 +0100

    Prepare v0.10.14.gtk34

--- autogen.sh
+++ autogen.sh
@@ -1,52 +1,23 @@
 #!/bin/sh
+#
+# $Id$
+#
+# Copyright (c) 2002-2010
+#         The Xfce development team. All rights reserved.
+#
+# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
+#
 
-set -e
-
-# script to refresh hard links before committing changes
-
-# package name
-pkgname=$(grep -m 1 'pkgname=' cache/PKGBUILD | cut -d= -f2)
-name=${pkgname%%-*}
-# first letter in uppercase
-name=$(echo ${name:0:1}|tr a-z A-Z)${name:1}
-
-target_dir=$(ls -1 | grep "$name-")
-# files or dir to copy
-path='
-gtk-2.0/gtk-widgets.rc
-gtk-2.0/apps/libreoffice.rc
-gtk-3.0/gtk-widgets.css
-gtk-3.0/panel.css
-'
-
-echo "path hard links = $path"
-
-function copy {
-	if [ "$1" == "" ]
-	then echo 'No files to copy, aborded.'
-	else
-		until [ $# = 0 ]
-		do
-			cp -f -l -r -v  $1 $(dirname "../$var/$1")
-			shift
-		done
-	fi
-	}
-
-function make {
-if [ "$1" != "" ]
-then
-until [ $# = 0 ]
-do
-	# target directory
-	var=$1
-	# action
-	copy $path
-	shift
-done
-else echo "no target directories for copying, aborded."
-fi
+(type xdt-autogen) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: You don't seem to have the Xfce development tools installed on
+            your system, which are required to build this software.
+            Please install the xfce4-dev-tools package first, it is available
+            from http://www.xfce.org/.
+EOF
+  exit 1
 }
 
-cd $name
-make $target_dir
+XDT_AUTOGEN_REQUIRED_VERSION="4.7.3" exec xdt-autogen $@
+
+# vi:set ts=2 sw=2 et ai:
